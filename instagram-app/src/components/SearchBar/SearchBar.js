daa5c962514f49bb847d9fd19d6c15bf4342e58d
josepaniagua <jpanjr@gmail.com>
2019-06-27 10:03:48 -0700

    aligined content in searchbar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -4,11 +4,13 @@ const SearchBar = () => {
     return (
         <div className='searchBarCont'>
             <div className='searchCenterCont'>
-                <div className='instaLogo'>
-                    <img src="https://img.icons8.com/material-outlined/96/000000/instagram-new.png"/>
-                </div>
-                <div className='instaWordCont'>
-                    <h1 className='instaWord'>Instagram</h1>
+                <div className='logoAndWord'>
+                    <div className='instaLogo'>
+                        <img src="https://img.icons8.com/material-outlined/96/000000/instagram-new.png"/>
+                    </div>
+                    <div className='instaWordCont'>
+                        <h1 className='instaWord'>Instagram</h1>
+                    </div>
                 </div>
                 <div className='instaSearch'>
                     <form>
