100400b6f711c2bc8da04209734f073697b843db
josepaniagua <jpanjr@gmail.com>
2019-06-26 18:44:00 -0700

    added instagram logo

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (
+        <div className='searchBarCont'>
+            <div className='searchCenterCont'>
+                <div className='instaLogo'>
+                    <img src="https://img.icons8.com/material-outlined/96/000000/instagram-new.png"/>
+                </div>
+                <div className='instaWord'></div>
+                <div className='instaSearch'></div>
+                <div className='instaIcons'></div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBar;
