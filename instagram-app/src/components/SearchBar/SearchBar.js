6c4e38792f9ef2534fe5b78738a766638a3e5dfc
josepaniagua <jpanjr@gmail.com>
2019-06-27 09:27:31 -0700

    mixins

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -7,9 +7,19 @@ const SearchBar = () => {
                 <div className='instaLogo'>
                     <img src="https://img.icons8.com/material-outlined/96/000000/instagram-new.png"/>
                 </div>
-                <div className='instaWord'></div>
-                <div className='instaSearch'></div>
-                <div className='instaIcons'></div>
+                <div className='instaWordCont'>
+                    <h1 className='instaWord'>Instagram</h1>
+                </div>
+                <div className='instaSearch'>
+                    <form>
+                        <input type='text' placeholder='Search' />
+                    </form>
+                </div>
+                <div className='instaIcons'>
+                    <img src="https://img.icons8.com/material-two-tone/48/000000/compass--v1.png"/>
+                    <img src="https://img.icons8.com/ios/50/000000/like.png"/>
+                    <img src="https://img.icons8.com/pastel-glyph/64/000000/person-male.png"/>
+                </div>
             </div>
         </div>
     );
