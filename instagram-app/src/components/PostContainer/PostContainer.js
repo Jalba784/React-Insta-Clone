bb95177bb4f451ade8882a8cdb51a20713607d4a
josepaniagua <jpanjr@gmail.com>
2019-06-28 01:48:12 -0700

    post skeleton

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,10 +3,22 @@ import React from 'react';
 const PostContainer = (props) => {
     return (
         <div>
-            <div className='userName'></div>
-            <div className='post-img'></div>
-            <div className='comment-sec'></div>
-            <div className='add-comment'></div>
+            <div className='userName'>
+                <img src={} /> // thumbNailImg
+                <h2>{}</h2> // user's name
+            </div>
+            <div className='post-img'>
+                <img src={} /> // main post image
+            </div>
+            <div className='comment-sec'>
+                // Add comment component here
+            </div>
+            <div className='add-comment'>
+                <form>
+                    <input placeholder='Add a comment...' />
+                    <img/> // 3 dots img
+                </form>
+            </div>
         </div>
     );
 };
