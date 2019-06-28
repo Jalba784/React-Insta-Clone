d1186177f747c3398139fc9a82c1c7ce6a1b0ad9
josepaniagua <jpanjr@gmail.com>
2019-06-28 01:37:20 -0700

    postcontainer scss

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,27 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
+import dummyData from "../../dummy-data.js";
 
-function App() {
-  return (
-    <div className="App">
-        <SearchBar/>
-      {/*<header className="App-header">*/}
-      {/*  <img src={logo} className="App-logo" alt="logo" />*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.js</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*  <a*/}
-      {/*    className="App-link"*/}
-      {/*    href="https://reactjs.org"*/}
-      {/*    target="_blank"*/}
-      {/*    rel="noopener noreferrer"*/}
-      {/*  >*/}
-      {/*    Learn React*/}
-      {/*  </a>*/}
-      {/*</header>*/}
-    </div>
-  );
+class App extends React.Component{
+    constructor() {
+        super();
+        this.state = {
+            gram: dummyData
+        }
+    }
+    render() {
+        return (
+            <div className="App">
+                <SearchBar/>
+                <PostContainer/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PostContainer = (props) => {
+    return (
+        <div>
+            <div className='userName'></div>
+            <div className='post-img'></div>
+            <div className='comment-sec'></div>
+            <div className='add-comment'></div>
+        </div>
+    );
+};
+
+export default PostContainer
